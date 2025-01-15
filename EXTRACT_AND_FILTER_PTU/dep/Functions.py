Auto roi and file roi displays on the images as overlay. fixed wron calculation of number of photons.

EXTRACT_AND_FILTER_PTU/dep/Functions.py
@@ -2156,30 +2156,46 @@ def extract_from_ptu(folder,ptu_file,LLim_ch_1,ULim_ch_1,LLim_ch_2,ULim_ch_2):
         channel_data = np.sum(flim_data_stack[:,:,channels[channel],:],axis=2)
 
         dpg.configure_item('loading_status',label='Exporting data to png - Channel '+str(channel+1))
-        to_png = channel_data*1/(np.max(channel_data)/255)
-
-        px = 1/plt.rcParams['figure.dpi']
-        width = channel_data.shape[0]
-        height = channel_data.shape[1]
 
 
-        fig = Figure(figsize=(width*px,height*px),facecolor='black')
-        ax = fig.add_subplot()
-        fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-        ax.margins(0, 0,)
-        ax.axis('off')
-        ax.imshow(to_png,cmap='nipy_spectral')
-        b =BytesIO()
-        FigureCanvas(fig).print_png(b)
-        plt.close()
 
-        b.seek(0)
-        image=Image.open(b)
-        image.save(os.path.join(folder,png_FC_name))
-        del(b)
+        
+        # to_png = channel_data*1/(np.max(channel_data)/255)
 
-        im = Image.fromarray(np.uint8(to_png))
-        im.save(os.path.join(folder,png_name))
+        # px = 1/plt.rcParams['figure.dpi']
+        # width = channel_data.shape[0]
+        # height = channel_data.shape[1]
+
+
+        # fig = Figure(figsize=(width*px,height*px),facecolor='black')
+        # ax = fig.add_subplot()
+        # fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        # ax.margins(0, 0,)
+        # ax.axis('off')
+        # ax.imshow(to_png,cmap='nipy_spectral')
+        # b =BytesIO()
+        # FigureCanvas(fig).print_png(b)
+        # plt.close()
+
+        # b.seek(0)
+        # image=Image.open(b)
+        # image.save(os.path.join(folder,png_FC_name))
+        # del(b)
+
+        # im = Image.fromarray(np.uint8(to_png))
+        # im.save(os.path.join(folder,png_name))
+
+        to_png = (channel_data / np.max(channel_data) * 255).astype(np.uint8)
+        # to_png_8bit = cv2.normalize(to_png, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+
+        colormap = cv2.COLORMAP_JET
+
+        colored_image = cv2.applyColorMap(to_png, colormap)
+
+        cv2.imwrite(os.path.join(folder, png_FC_name), colored_image)
+
+        cv2.imwrite(os.path.join(folder, png_name), to_pngpyth)
+
         
         export_df = pd.DataFrame(channel_data)
         # dpg.configure_item('loading_status',label='Exporting data to csv - Channel '+str(channel+1))

Phot2Conc/dep/.ipynb_checkpoints/Texture_registry-checkpoint.py
@@ -109,20 +109,36 @@ Current_image_1 = NO_IMAGE_INTENSITY
 Current_image_2 = NO_IMAGE_INTENSITY
 
 
+processor_1 = ImageROIProcessor()
+processor_1.image=(NO_IMAGE_INTENSITY*255).astype(np.uint8)
+
+processor_2 = ImageROIProcessor()
+processor_2.image=(NO_IMAGE_INTENSITY*255).astype(np.uint8)
+
+rgba_image_1 = im_to_rgbim(processor_1.image.astype(np.uint8))
+rgba_image_2 = im_to_rgbim(processor_2.image.astype(np.uint8))
+
+rgba_image_1  =cv2.resize(rgba_image_1, (w, h), interpolation=cv2.INTER_CUBIC)
+rgba_image_2  =cv2.resize(rgba_image_2, (w, h), interpolation=cv2.INTER_CUBIC)
 
 
+dpg_image_1=(rgba_image_1.astype(np.float32) /np.max(processor_1.image.astype(np.uint8))).flatten().tolist()
+dpg_image_2=(rgba_image_2.astype(np.float32) /np.max(processor_2.image.astype(np.uint8))).flatten().tolist()
 
+# dpg.set_value(tex_name, new_texture_data)
 
+# _update_textures_both_roi('ch1',None)
+# _update_textures_both_roi('ch2',None)
 
 # image_1=image_INT_LT(Current_image_1,w,h)
 # image_2=image_INT_LT(Current_image_2,w,h)
 
-image_1=plot_IMAGE(Current_image_1,w,h)
-image_2=plot_IMAGE(Current_image_2,w,h)
+# image_1=plot_IMAGE(Current_image_1,w,h)
+# image_2=plot_IMAGE(Current_image_2,w,h)
 
 
 
-dpg_image_1 = convert_to_texture(image_1)
+# dpg_image_1 = convert_to_texture(image_1)
 # dpg_image_1 = []
 # for i in range(0, image_1._size[1]):
 #     for j in range(0, image_1._size[0]):
@@ -143,7 +159,7 @@ dpg_image_1 = convert_to_texture(image_1)
 #         dpg_image_2.append(pixel[2]/255)
 #         dpg_image_2.append(255/255)
 
-dpg_image_2 = convert_to_texture(image_2)
+# dpg_image_2 = convert_to_texture(image_2)
 
 
 

Phot2Conc/dep/.ipynb_checkpoints/automated_roi-checkpoint.py
@@ -75,10 +75,11 @@ class ImageROIProcessor:
         # Znajdowanie konturów
         contours, hierarchy = cv2.findContours(thresholded, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
         if not contours or hierarchy is None:
-            raise ValueError("Nie znaleziono konturów w obrazie.")
-
+            # raise ValueError("Nie znaleziono konturów w obrazie.")
+            external_mask = self._create_external_mask(None, image_to_process.shape)
+        else:
         # Klasyfikacja konturów
-        external_contour, internal_contour = self._classify_contours_by_area(contours, hierarchy)
+            external_contour, internal_contour = self._classify_contours_by_area(contours, hierarchy)
 
         # Dopasowanie elipsy i stworzenie maski ROI
         # ellipse_mask = np.zeros(image_to_processshape, dtype=np.uint8)
@@ -86,7 +87,7 @@ class ImageROIProcessor:
         #     ellipse_mask, _ = self._fit_ellipse_to_contour(internal_contour, image_to_process.shape)
 
         # Tworzenie maski zewnętrznego konturu
-        external_mask = self._create_external_mask(external_contour, image_to_process.shape)
+            external_mask = self._create_external_mask(external_contour, image_to_process.shape)
 
         # Łączenie masek w finalną ROI
         # roi_image = self._create_final_mask(external_mask, ellipse_mask, image_to_process.shape)